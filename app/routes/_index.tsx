9266ee59e693fce6a6493fe2716776954315964e
Ameer9666 <ameerbasham297@gmail.com>
2024-08-27 14:06:55 +0530

    Remove default template and update port and meta title and description

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -2,47 +2,19 @@ import type { MetaFunction } from "@remix-run/node";
 
 export const meta: MetaFunction = () => {
   return [
-    { title: "New Remix App" },
-    { name: "description", content: "Welcome to Remix!" },
+    { title: "Sunvoy Affiliate Revenue Calculator" },
+    {
+      name: "description",
+      content:
+        "The Sunvoy Affiliate Revenue Calculator is a dynamic, interactive tool designed to help potential affiliates forecast their earnings over a 12-month period.",
+    },
   ];
 };
 
 export default function Index() {
   return (
-    <div className="font-sans p-4">
-      <h1 className="text-3xl">Welcome to Remix</h1>
-      <ul className="list-disc mt-4 pl-6 space-y-2">
-        <li>
-          <a
-            className="text-blue-700 underline visited:text-purple-900"
-            target="_blank"
-            href="https://remix.run/start/quickstart"
-            rel="noreferrer"
-          >
-            5m Quick Start
-          </a>
-        </li>
-        <li>
-          <a
-            className="text-blue-700 underline visited:text-purple-900"
-            target="_blank"
-            href="https://remix.run/start/tutorial"
-            rel="noreferrer"
-          >
-            30m Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            className="text-blue-700 underline visited:text-purple-900"
-            target="_blank"
-            href="https://remix.run/docs"
-            rel="noreferrer"
-          >
-            Remix Docs
-          </a>
-        </li>
-      </ul>
+    <div>
+      <h1>Sunvoy Affiliate Revenue Calculator</h1>
     </div>
   );
 }
